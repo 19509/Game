e71848363d2a4bbe2e13c2c1be790f96caffbc99
James Hicks <19509@my4county.net>
2021-05-10 10:48:39 -0400

    added level between 2 and 3 for better float cube entry course.

---
+++ Middle_Man/Assets/ColChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColChange : MonoBehaviour
+{
+    public Material op1;
+    public Material op2;
+    public bool blue = true;
+
+    void update()
+    {
+        if (blue == true)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                GetComponent<Renderer>().material = op2;
+            }
+        }
+    }
+}
