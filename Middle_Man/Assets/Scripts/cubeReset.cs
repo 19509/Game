04c2fc8c98cfb47984dc8f1f23042519391daec6
James Hicks <19509@my4county.net>
2021-05-11 11:25:39 -0400

    Added catch fields for static blocks in all but level 6 and float levels

---
+++ Middle_Man/Assets/Scripts/cubeReset.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cubeReset : MonoBehaviour
+{
+    Scene scene;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("cubeReset"))
+        {
+            this.transform.parent = null;
+            if (scene.buildIndex == 1)
+            {
+                this.transform.position = new Vector3(-10.41f, 1.45f, -15.06f);
+            }
+            else if (scene.buildIndex == 2)
+            {
+                this.transform.position = new Vector3(-14.45f, 9.193f, -7.04f);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("cubeReset"))
+        {
+            this.transform.parent = null;
+            if (scene.buildIndex == 1)
+            {
+                this.transform.position = new Vector3(-10.41f, 1.45f, -15.06f);
+            }
+            else if (scene.buildIndex == 2)
+            {
+                this.transform.position = new Vector3(-14.45f, 9.193f, -7.04f);
+            }
+        }
+    }
+}
+
